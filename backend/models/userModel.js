810f7d435ff7fc1dbd8941872c692ae7d22e62be
Ahsan Irfan <theahsan_one@hotmail.com>
2022-07-07 05:33:45 +0500

    User model timestamps

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,19 +1,24 @@
 const mongoose = require("mongoose")
 
-const userSchema = mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, "Please add a name"],
-        unique: true,
+const userSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, "Please add a name"],
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: [true, "Please add a password"],
+        },
+        votes: {
+            type: [mongoose.Schema.Types.ObjectId],
+            ref: "Vote",
+        },
     },
-    password: {
-        type: String,
-        required: [true, "Please add a password"],
-    },
-    votes: {
-        type: [mongoose.Schema.Types.ObjectId],
-        ref: "Vote",
-    },
-})
+    {
+        timestamps: true,
+    }
+)
 
 module.exports = mongoose.model("User", userSchema)
