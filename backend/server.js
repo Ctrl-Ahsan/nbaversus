dffdc0d6928c1835f6ade14520301b92e526e515
Ahsan Irfan <theahsan_one@hotmail.com>
2023-05-12 17:02:11 -0400

    CORS to allow external API requests from Heroku

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const cors = require("cors")
 const rateLimit = require("express-rate-limit")
 const path = require("path")
 require("dotenv").config()
@@ -19,6 +20,7 @@ const limiter = rateLimit({
 
 // configure
 app.enable("trust proxy")
+app.use(cors())
 app.use(limiter)
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
