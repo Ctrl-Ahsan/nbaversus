d99410897fc9f0a39ec9c2b97d888e01408e539e
Ahsan Irfan <theahsan_one@hotmail.com>
2024-11-20 02:13:18 +0100

    Database for dev

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,12 @@ const mongoose = require("mongoose")
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI)
+        const uri =
+            process.env.NODE_ENV === "development"
+                ? process.env.MONGO_URI_DEV
+                : process.env.MONGO_URI
+
+        const conn = await mongoose.connect(uri)
         console.log(`MongoDB connected: ${conn.connection.host}`.cyan)
     } catch (error) {
         console.log(error.red)
