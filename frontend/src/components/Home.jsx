9493c26a142ea34a4d958646a5d93e157a953903
Ahsan Irfan <theahsan_one@hotmail.com>
2024-11-22 14:19:31 +0100

    Puff-in animation for vote percentage

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -97,7 +97,7 @@ const Home = () => {
                 >
                     <div className="name">{question.players[0].name}</div>
                     <div
-                        className={`votes ${voted ? "fade-in" : ""}`}
+                        className={`votes ${voted ? "puff-in-center" : ""}`}
                         style={{ opacity: voted ? "100%" : "0%" }}
                     >
                         {percentage1}%
