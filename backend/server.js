30e80b051c7a5ada48530f2d0f59523e171225cc
Ahsan Irfan <theahsan_one@hotmail.com>
2024-11-24 13:42:19 +0100

    Update error handling middleware

--- backend/server.js
+++ backend/server.js
@@ -24,8 +24,8 @@ app.use(cors())
 app.use(limiter)
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(errorHandler)
 app.use("/api", require("./routes"))
+app.use(errorHandler)
 
 // Serve frontend
 if (process.env.NODE_ENV === "production") {
