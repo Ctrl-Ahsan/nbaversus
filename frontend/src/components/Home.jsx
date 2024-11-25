41fca43198c289052989979d4522b4509adc97c1
Ahsan Irfan <theahsan_one@hotmail.com>
2024-11-25 15:28:49 +0100

    Connect daily questions to frontend

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -2,84 +2,69 @@ import { useEffect, useState } from "react"
 import "./Home.css"
 import { teamColors } from "../config"
 import { GiGoat } from "react-icons/gi"
+import axios from "axios"
+import { toast } from "react-toastify"
 
 const Home = () => {
     const [versus, setVersus] = useState([])
 
     useEffect(() => {
-        const response = [
-            {
-                category: "GOAT",
-                players: [
-                    {
-                        name: "LeBron James",
-                        personId: 2544,
-                        teamId: 1610612747,
-                        votes: 22,
-                    },
-                    {
-                        name: "Michael Jordan",
-                        personId: 893,
-                        teamId: 1610612741,
-                        votes: 8,
-                    },
-                ],
-            },
-            {
-                category: "Shooting",
-                players: [
-                    {
-                        name: "LeBron James",
-                        personId: 2544,
-                        teamId: 1610612747,
-                        votes: 2,
-                    },
-                    {
-                        name: "Stephen Curry",
-                        personId: 201939,
-                        teamId: 1610612744,
-                        votes: 8,
-                    },
-                ],
-            },
-        ]
-        setVersus(response)
+        const fetchDailyQuestions = async () => {
+            try {
+                const { data } = await axios
+                    .get("/api/questions/daily")
+                    .catch((error) => {
+                        toast.error(error.response.data)
+                    })
+
+                setVersus(data.questions)
+                console.log(data)
+            } catch (error) {
+                console.error("Error fetching daily questions:", error)
+            }
+        }
+        fetchDailyQuestions()
     }, [])
 
-    const Panel = ({ question }) => {
+    const Panel = ({ players, votes }) => {
         const [voted, setVoted] = useState(false)
         const [p1Wins, setP1Wins] = useState(false)
         const [p2Wins, setP2Wins] = useState(false)
+        const [votes1, setVotes1] = useState(votes.player1)
+        const [votes2, setVotes2] = useState(votes.player2)
 
-        const votes1 = question.players[0].votes
-        const votes2 = question.players[1].votes
+        const player1 = players.player1
+        const player2 = players.player2
         const totalVotes = votes1 + votes2
 
-        const percentage1 = Math.round((votes1 / totalVotes) * 100)
-        const percentage2 = 100 - percentage1
+        // Calculate percentages only when there are votes
+        const percentage1 =
+            totalVotes === 0 ? 0 : Math.round((votes1 / totalVotes) * 100)
+        const percentage2 = totalVotes === 0 ? 0 : 100 - percentage1
 
+        // Dynamically calculate widths
         const width1 = voted
             ? percentage1 > percentage2
-                ? percentage1 + 50
+                ? percentage1 + 30
                 : 50
             : 50
         const width2 = voted
             ? percentage2 > percentage1
-                ? percentage2 + 50
+                ? percentage2 + 30
                 : 50
             : 50
 
-        const img1 = `https://cdn.nba.com/headshots/nba/latest/1040x760/${question.players[0].personId}.png`
-        const img2 = `https://cdn.nba.com/headshots/nba/latest/1040x760/${question.players[1].personId}.png`
-        const logo1 = `https://cdn.nba.com/logos/nba/${question.players[0].teamId}/global/L/logo.svg`
-        const logo2 = `https://cdn.nba.com/logos/nba/${question.players[1].teamId}/global/L/logo.svg`
-        const bg1 = teamColors[question.players[0].teamId]
-        const bg2 = teamColors[question.players[1].teamId]
+        const img1 = `https://cdn.nba.com/headshots/nba/latest/1040x760/${player1.personId}.png`
+        const img2 = `https://cdn.nba.com/headshots/nba/latest/1040x760/${player2.personId}.png`
+        const logo1 = `https://cdn.nba.com/logos/nba/${player1.teamId}/global/L/logo.svg`
+        const logo2 = `https://cdn.nba.com/logos/nba/${player2.teamId}/global/L/logo.svg`
+        const bg1 = teamColors[player1.teamId]
+        const bg2 = teamColors[player2.teamId]
 
         // Extract first and last names
-        const fullName1 = question.players[0].name
+        const fullName1 = player1.name
         const lastName1 = fullName1.split(" ").slice(-1).join(" ")
-        const fullName2 = question.players[1].name
+        const fullName2 = player2.name
         const lastName2 = fullName2.split(" ").slice(-1).join(" ")
 
         // Decide name display based on width
@@ -88,12 +73,14 @@ const Home = () => {
 
         const handleClick1 = () => {
             if (!p1Wins && !p2Wins) {
+                setVotes1(votes1 + 1)
                 setVoted(true)
                 setP1Wins(true)
             }
         }
         const handleClick2 = () => {
             if (!p1Wins && !p2Wins) {
+                setVotes2(votes2 + 1)
                 setVoted(true)
                 setP2Wins(true)
             }
@@ -118,7 +105,7 @@ const Home = () => {
                         className={`votes ${voted ? "puff-in-center" : ""}`}
                         style={{ opacity: voted ? "100%" : "0%" }}
                     >
-                        {percentage1}%
+                        {`${percentage1}%`}
                     </div>
                     <div className="image">
                         <img src={img1} />
@@ -142,7 +129,7 @@ const Home = () => {
                         className={`votes ${voted ? "puff-in-center" : ""}`}
                         style={{ opacity: voted ? "100%" : "0%" }}
                     >
-                        {percentage2}%
+                        {`${percentage2}%`}
                     </div>
                     <div className="image">
                         <img src={img2} />
@@ -160,15 +147,16 @@ const Home = () => {
                 <img src="/nbaversus.png" alt="" />
             </div>
             <div className="content">
-                {versus.map((question, index) => (
+                {versus?.map((category, index) => (
                     <div className="versus" key={index}>
                         <div className="category">
-                            {question.category}
-                            {question.category === "GOAT" ? <GiGoat /> : ""}
+                            {category.question}
+                            {category.question === "GOAT" ? <GiGoat /> : ""}
                         </div>
                         <Panel
-                            question={question}
-                            key={`${question.category}-${index}`}
+                            players={category.players}
+                            votes={category.votes}
+                            key={`${category.category}-${index}`}
                         />
                     </div>
                 ))}
